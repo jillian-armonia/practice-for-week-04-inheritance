613dad44ac0fb85ddde7d3502f7c3b2908da0704
Jillian Armonia <jillian.marice@gmail.com>
2024-05-10 15:24:37 +0900

    created student and teacher classes

--- classes/student.js
+++ classes/student.js
@@ -1,5 +1,25 @@
 const Person = require('./person');
 
+class Student extends Person{
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2){
+    let GPA1 = student1.GPA;
+    let GPA2 = student2.GPA;
+
+    if (GPA1 > GPA2){
+      return `${student1.firstName} ${student1.lastName} has the higher GPA.`
+    } else if (GPA2 > GPA1){
+      return `${student2.firstName} ${student2.lastName} has the higher GPA.`
+    } else if (GPA1 === GPA2) {
+      return 'Both students have the same GPA'
+    }
+  }
+}
 // Your code here
 
 /****************************************************************************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,5 +1,16 @@
 const Person = require('./person');
 
+class Teacher extends Person{
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers){
+    return teachers.reduce((sum, teacher) => sum + teacher.yearsOfExperience, 0);
+  }
+}
 // Your code here
 
 /****************************************************************************/
